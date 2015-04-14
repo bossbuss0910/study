2feefce9c6ce79e80a296f9ecfbba4a6d6c66b3d
bossbuss0910 <bumpof8265chicke@yahoo.co.jp>
2015-04-15 00:36:18 +0900

    init

--- LanguageProcessing/data.py
+++ LanguageProcessing/data.py
@@ -1,21 +1,35 @@
 #coding=utf-8
 from collections import Counter
+import os
+
 word_data=[]
 
 def file_input(path):
 	f=open(path)
+	data=f.read()
+	f.close()
 	word_list=[]
-	for line in f:
+	lines=data.split("\n")
+	for line in lines:
 		word_list=line.split(",")
 		for index in range(2,len(word_list)):
-			word_data.append(word_list(index))
+			word_data.append(word_list[index])
 		word_list=[]
+
+def file_output(path):
+	f=open(path,"w")
+	counter = Counter(word_data)
+	for word, cnt in counter.most_common():
+		print word,cnt
+		f.write("{0},{1}\n".format(word,cnt))
 	f.close()
 
 for index in range(1,51):
-	input_filename='output/wordlist%d.txt'%index
+	input_filename='output/wordlist%d.csv'%index
 	input_path = os.path.abspath(input_filename)
+
 	file_input(input_path)
-counter = Counter(word_data)
-for word, cnt in counter.most_common():
-	    print word, cnt
+	output_filename='output/wordcount.csv'
+output_filename='output/wordcount.csv'
+output_path= os.path.abspath(output_filename)
+file_output(output_path)
