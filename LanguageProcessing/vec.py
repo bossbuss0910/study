1a5bb9e91dd2f200d935fc85bc66d1a73ef05644
bossbuss0910 <bumpof8265chicke@yahoo.co.jp>
2015-04-17 15:39:59 +0900

    initial

--- LanguageProcessing/vec.py
+++ LanguageProcessing/vec.py
@@ -1,5 +1,5 @@
-#codeing=utf-8
-
+# -*- coding: utf-8 -*-
+import os
 word_data=[]
 
 def file_input(path):
@@ -9,7 +9,6 @@ def file_input(path):
 	word_list=[]
 	lines=data.split("\n")
 	for line in lines:
-		print line
 		word_list=line.split(",")
 		for index in range(2,len(word_list)):
 			if word_list[index] not in ["値段","価格"]:
@@ -18,7 +17,7 @@ def file_input(path):
 word_vec_c={}
 word_vec_e={}
 
-def vec(word_vec):
+def vec(word_vec,path):
 	f=open(path)
 	data=f.read()
 	f.close()
@@ -27,28 +26,30 @@ def vec(word_vec):
 	for line in lines:
 		word_list=line.split(",")
 		if "安い" in word_list:
-			for index range(2,len(word_list)):
-				word_vector[word_vec.index(word_list[index])]=1
-			word_vec_c.update(word_list[0],word_vector)
+			for index in range(2,len(word_list)):
+				if word_list[index] not in ["値段","価格"]:
+					word_vector[word_vec.index(word_list[index])]=1
+			word_vec_c.update({word_list[0]:word_vector})
 		if "高い" in word_list:
-			for index range(2,len(word_list)):
-				word_vector[word_vec.index(word_list[index])]=1
-			word_vec_e.update(word_list[0],word_vector)
+			for index in range(2,len(word_list)):
+				if word_list[index] not in ["値段","価格"]:
+					word_vector[word_vec.index(word_list[index])]=1
+			word_vec_e.update({word_list[0]:word_vector})
 
-for index in range(1,1):
+for index in range(1,2):
 	input_filename='output/wordlist%d.csv'%index
 	input_path = os.path.abspath(input_filename)
 	file_input(input_path)
 	word_vec=[]
-	for word in set(wprd_data):
+	for word in set(word_data):
 		print word
 		word_vec.append(word)
-	vec(word_vec)
+	vec(word_vec,input_path)
 
 	print"安い"
-	for K,V in sorted(word_vec_c):
-		print K,V
+	for K,V in sorted(word_vec_c.items()):
+		print "a"
 
 	print"高い"
-	for K,V in sorted(word_vec_e):
-		pritn K,V
+	for K,V in sorted(word_vec_e.items()):
+		print "b"
