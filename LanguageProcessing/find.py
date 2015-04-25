04dbee3061c6410f4f5b7bb53dfa58d0ed135c2c
bossbuss0910 <bumpof8265chicke@yahoo.co.jp>
2015-04-25 17:44:36 +0900

    init

--- LanguageProcessing/find.py
+++ LanguageProcessing/find.py
@@ -2,10 +2,12 @@
 
 import re
 import os
+import json
+
 
 current_dir="~/amazon_review"
 input_filename=current_dir+"/price_review.json"
-
+output_filename=current_dir+"/price_sence.txt"
 expensive_list=["expensive"]
 cheap_list=["cheap"]
 
@@ -16,12 +18,13 @@ def sentence_split(target):
 
 def price_sence(price_review_dic):
 	price_sence_dic={}
-	print price_review_dic
+	review_num=0
 	for price,review in price_review_dic.items():
 		e_count=0
 		c_count=0
 		price_sence_list=[]
 		for sentence in review:
+			review_num+=1
 			check_count=0
 			if len(list(set(expensive_list)&set(sentence_split(sentence))))>0:
 				e_count=e_count+1
@@ -36,6 +39,7 @@ def price_sence(price_review_dic):
 			price_sence_list=[e_count,c_count]
 		price_sence_dic[price]=price_sence_list
 	print price_sence_dic
+	print "review_num:%d"%review_num
 	return price_sence_dic
 
 def price_match(dic):
@@ -46,20 +50,47 @@ def price_match(dic):
 				price_review_dic[price]=review
 			else:
 				price_review_dic[price].extend(review)
-	print price_review_dic
 	return price_review_dic
 
 def input_file(path):
+	with open(path,'r') as f:
+		dic =json.load(f)
+	return dic
+	
+def output_file(dic,path):
+	f=open(path,"w")
+	for price in sorted(dic.keys()):
+		out_str=str(price)+" "
+		for number in dic[price]:
+			out_str += str(number)+" "
+		f.write(out_str.strip()+"\n")
+	f.close()
+	
 
 def main():
 	file_path=os.path.expanduser(input_filename)
-	itemid_dic={}
+	outfile_path=os.path.expanduser(output_filename)
+
 	itemid_dic=input_file(file_path)
-	
+	price_review=price_match(itemid_dic)
+	price_sence_dic=price_sence(price_review)
+	ex_num=0
+	ch_num=0
+	price_list=[]
+	price_sence_dic2={}
+	for price,num_list in price_sence_dic.items():
+		price_list.append(float(price))
+		price_sence_dic2[float(price)]=num_list
+		ex_num+=num_list[0]
+		ch_num+=num_list[1]
+		if sum(num_list)!=0:
+			print price
+			print num_list
+	price_list.sort()
+	print "low:%f	high:%f	ave:%f"%(price_list[0],price_list[len(price_list)-1],sum(price_list)/len(price_list))
+	print "expensive:%d	cheap:%d"%(ex_num,ch_num)
+	output_file(price_sence_dic2,outfile_path)
 
 if __name__=="__main__":
 	main()
 
-
-dic={"1":{"100":["this is cheap","expensive yeah!","cheap but expensive"]},"2":{"100":["this is cheap","expensive yeah!","cheap but expensive","expensive expensive"]}}
-price_match(dic)
