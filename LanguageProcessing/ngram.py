ad05e2e952fbab1f66e8a05010c873054b428995
bossbuss0910 <Boss@nakano-mobile72-45.mind.meiji.ac.jp>
2015-06-04 17:33:29 +0900

    commit

--- LanguageProcessing/ngram.py
+++ LanguageProcessing/ngram.py
@@ -19,6 +19,9 @@ def wakati(text):
 #各reviewのマップ
 user_map={}
 
+#星の数
+star_list=[]
+
 #N_gram抽出
 def ngram(word_list,N,review_id):
 	w_list=[]
@@ -40,17 +43,26 @@ def text_split(text):
 	review_list=[]
 	str=""
 	line=f.readline()
+	star_list.append(line[0])
 	while line:
-		if line.startswith('【'):
+		if line.startswith('【review'):
 			review_list.append(str)
 			str=""
 			line=f.readline()
+			star_list.append(line[0])
 		else:
 			str=str+line
 		line=f.readline()
 	review_list.append(str)
 	f.close()
 	return review_list
+
+#ファイル出力
+def output_file(filename,output_list):
+	f=open(filename,'w')
+	for str in output_list:
+		f.write(str+"\n")
+	f.close()
 
 
 def main(N,text,review_id):
@@ -60,19 +72,21 @@ def main(N,text,review_id):
 
 if __name__ == "__main__":
 
-	filename='dic/review2.txt'
-
+	input_filename='dic/review2.txt'
+	output_filename='output/wordlist2.csv'
 	#レビュー数
 	review_number=0
 	#値段or価格を含むレビュー数
 	target_review=0
 
-	text = os.path.abspath(filename)
+	text = os.path.abspath(input_filename)
 	text_list=text_split(text)
 	for index,text in enumerate(text_list):
 		main(3,text,index)
-
+	
 	review_number=index
+	#ファイルに書き込むためのリスト
+	output_list=[]
 
 	for K,V in user_map.items():
 		word_list=[]
@@ -81,8 +95,13 @@ if __name__ == "__main__":
 				target_review += 1
 				for word in word_set:
 					word_list.append(word)
-		print K+1
-		print ",".join(set(word_list))
-	print 
+		if len(word_list)!=0:
+			output_list.append("{0},{1},{2}".format(K+1,star_list[K-1],",".join(set(word_list))))
+	for s in output_list:
+		print s
+
+	output_filename=os.path.abspath(output_filename)
+
+	output_file(output_filename,output_list)
 	print "レビュー数:%d" %review_number
 	print "値段・価格を含むレビュー数:%d" %target_review
